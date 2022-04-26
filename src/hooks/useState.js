bb4671380fa5bfefcef19d45af16ec670427dddb
victor misiko <victormisiko.vm@gmail.com>
2022-04-26 16:26:59 +0300

    Added state hooks

---
+++ src/hooks/useState.js
@@ -0,0 +1,23 @@
+import { reactive } from "vue";
+
+export function useState() {
+  const state = reactive({
+    currency: "KES",
+    amount: 0.0,
+    loading: false,
+    defaultPaymentMethod: null,
+    transaction_id: null,
+    poll_count: 0,
+    poll_limit: 30,
+    errorText: "",
+    mpesaCode: "",
+    startResponseTime: null,
+    showTransactionLimit: false,
+    showAdditionalCardFields: false,
+    additionalData: null,
+    showErrorModal: false,
+    loadingText: "please wait",
+  });
+
+  return state;
+}
