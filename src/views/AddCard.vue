7c2c743c436af160fee1e172b17d870e70b7844c
victor misiko <misiko@sendyit.com>
2022-06-03 16:01:49 +0300

    Fixed undefined in collect card details endpoint

--- src/views/AddCard.vue
+++ src/views/AddCard.vue
@@ -231,7 +231,7 @@ export default {
         {
           data: newCardPayload,
           headers: {
-            Authorization: getBupayload.authToken,
+            Authorization: getBupayload.value.authToken,
           },
         },
         (status, response) => {
