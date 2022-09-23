e73b3ab255f4ff8bd774e883996a5c8156339814
victor misiko <victormisiko.vm@gmail.com>
2022-09-23 18:54:38 +0300

    Added confirmDetails modal

--- src/views/ManageWIthdrawalOptions/BankWIthdrawal.vue
+++ src/views/ManageWIthdrawalOptions/BankWIthdrawal.vue
@@ -1,3 +1,84 @@
 <template>
+  <div class="flex-center">
+
+    <div class="card">
+      <TopInfo :icon="icon" :title="$translate('bank')">
+        <template v-slot:subtitle>
+          <span class="body-2-regular text-gray70">
+            Enter your bank account details
+          </span>
+        </template>
+      </TopInfo>
+
+      <div class="mgt-5">
+
+        <div class="textfield mgt-5">
+          <label for="" class="normal-text"> Bank Name</label>
+          <select
+            v-model="bankName"
+            class="phone-input"
+            required
+          >   
+            <option selected> Select your bank </option>
+            <option value="kcb"> KCB</option>
+            <option value="coorp"> CO-ORP</option>
+            <option value="equity"> Equity</option>
+          </select>
+        </div>
+
+        <div class="textfield mgt-5">
+          <label for="" class="normal-text"> Account Name</label>
+          <input
+            type="text"
+            v-model="accountName"
+            class="phone-input"
+            placeholder="Enter your account name"
+          /> 
+        </div>
+
+        <div class="textfield mgt-5">
+          <label for="" class="normal-text"> Account Number</label>
+          <input
+            type="text"
+            v-model="accountNumber"
+            class="phone-input"
+            placeholder="Enter your account number"
+          /> 
+        </div>
+      </div>
+
+      <div class="mgy-10"></div>
+
+      <div>
+        <sendy-btn
+        @click="handleConfirm"
+        color="primary"
+        :block="true"
+        text="Continue"
+        />
+      </div>
+    </div>
+    <ConfirmDetailsModal :show="confirm"  @close="confirm=false"/>
+  </div>
   
 </template>
+
+<script setup>
+import { ref } from 'vue';
+import TopInfo from '../../components/topInfo.vue';
+import ConfirmDetailsModal from './components/ConfirmDetails.vue';
+
+const icon = ref('back');
+const confirm = ref(false);
+const accountName = ref('');
+const bankName = ref('');
+const accountNumber = ref('');
+
+const handleConfirm = () => {
+  console.log('handleConfirm');
+  confirm.value = true;
+  console.log('handleConfirm', confirm.value);
+
+};
+
+</script>
---
+++ src/views/ManageWIthdrawalOptions/components/ConfirmDetails.vue
@@ -0,0 +1,138 @@
+<template>
+  <div id="cvv-modal" class="modal" ref="confirmModal">
+    <div class="modal-content">
+      <div class="mgt-4">
+        
+        <div>
+          <span class="text-subtitle-1"
+            > Confirm Details </span
+          >
+        </div>
+
+        <div class="text-body-2 mgt-4">
+          <span>Please review your payout details to ensure that they are entered correctly.</span>
+        </div>
+
+        <div class="direction-flex mgt-5">
+          <IconView icon="bank"  class="mgy-auto"/>
+          <div class="mgl-4">
+            <span class="text-caption-1 semi-bold text-gray90">Bank Name</span>: <span class="text-caption-1">Absa Bank</span> <br/>
+            <span class="text-caption-1 semi-bold text-gray90">Acc Name</span>: <span class="text-caption-1">Arabica Coffee</span> <br/>
+            <span class="text-caption-1 semi-bold text-gray90">Acc No</span>: <span class="text-caption-1">010002345678910</span> <br/>
+          </div>
+        </div>
+
+      </div>
+
+      <sendy-btn
+        :block="true"
+        color="primary"
+        class="mgt-8"
+        type="submit"
+        @click="$emit('close')"
+      >
+        Save Withdrawal Details
+      </sendy-btn>
+
+      <div class="mgt-5 text-center">
+        <span @click="$emit('close')" class="text-midnightBlue20 cursor pointer"> Cancel </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref, watch } from "vue";
+const confirmModal = ref(null);
+
+const props = defineProps(["show"]);
+const emit = defineEmits(['close']);
+
+
+watch(() => props.show, (val) => {
+  val ? handleOpen() : handleClose();
+});
+
+onMounted(() => {
+  console.log(props.show);
+  props.show ? handleOpen() : handleClose();
+})
+
+const handleOpen = () => {
+  confirmModal.value.style.display = "block";
+};
+
+const handleClose = () => {
+  confirmModal.value.style.display  = "none";
+};
+</script>
+
+<style lang="scss">
+.modal {
+  display: none;
+  position: fixed;
+  z-index: 1;
+  padding-top: 150px;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgb(0, 0, 0);
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+/* Modal Content */
+.modal-content {
+  position: relative;
+  background-color: #fefefe;
+  margin: auto;
+  padding: 32px;
+  border: 1px solid #888;
+  border-radius: 4px;
+  width: 300px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  -webkit-animation-name: animatetop;
+  -webkit-animation-duration: 0.4s;
+  animation-name: animatetop;
+  animation-duration: 0.4s;
+}
+
+/* Add Animation */
+@-webkit-keyframes animatetop {
+  from {
+    top: -300px;
+    opacity: 0;
+  }
+  to {
+    top: 0;
+    opacity: 1;
+  }
+}
+
+@keyframes animatetop {
+  from {
+    top: -300px;
+    opacity: 0;
+  }
+  to {
+    top: 0;
+    opacity: 1;
+  }
+}
+
+/* The Close Button */
+.close {
+  color: black;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: #000;
+  text-decoration: none;
+  cursor: pointer;
+}
+</style>
