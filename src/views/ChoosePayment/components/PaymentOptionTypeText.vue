55108a983468db537ae89112855cbe05c0b93352
victor misiko <victormisiko.vm@gmail.com>
2022-12-16 13:24:19 +0300

    Hide available balance on checkout for banktransfer

--- src/views/ChoosePayment/components/PaymentOptionTypeText.vue
+++ src/views/ChoosePayment/components/PaymentOptionTypeText.vue
@@ -9,7 +9,7 @@
     <div v-if="paymentOption.pay_method_id === 20" >
         <div class="mgy-auto">
           <span> {{ $translate('pay_by_bank') }}</span>
-          <div class="caption-2-semibold text-gray70 direction-flex" v-if="(getBupayload.pay_direction !== 'PAY_ON_DELIVERY' && hideAvalailablebalance)">
+          <div class="caption-2-semibold text-gray70 direction-flex" v-if="(getBupayload.pay_direction !== 'PAY_ON_DELIVERY' && !hideAvalailablebalance)">
             <span> {{  $translate('available_balance') }}</span>
 
             <IconView
@@ -45,6 +45,7 @@ const { getBupayload } = useState();
 const isCheckout = computed(() =>  route.name === 'Entry');
 
 const hideAvalailablebalance = computed(() => {
+  console.log(route.name);
   return route.name === 'Entry' || route.name === 'FailedView' || route.name === 'SuccessView';
 } );
 
