1891f59bfd46964e23f8381acb01a26670fe541d
victor misiko <victormisiko.vm@gmail.com>
2023-03-11 23:26:07 +0300

    STP-284: Add payment Status Model and payments datasource

---
+++ src/Models/payment/paymentStatus.js
@@ -0,0 +1,23 @@
+
+class PaymentStatus {
+  constructor (payload) {
+    this.transaction_status = payload.transaction_status;
+    this.code = payload.code;
+    this.status = payload.status;
+    this.message = payload.message;
+    this.receipt_no = payload.receipt_no;
+    this.transaction_id = payload.transaction_id;
+    this.sendy_error_code = payload.sendy_error_code;
+  };
+
+  static DS_TIMEOUT_ERRORS = [
+    "DS_TIMEOUT",
+  ];
+  
+  get isDstimeout() {
+    return this.DS_TIMEOUT_ERRORS.includes(this.sendy_error_code);
+  }
+
+}
+
+export default PaymentStatus;
