2a6b2a9c2c506c13014089a3f47dc8abd74eade0
Misiko <misiko@Jeremys-MacBook-Pro-2.local>
2023-03-22 12:49:46 +0300

    SPT-282: Fixed jest.cofig .js for code coverage deployment format

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
-  transformIgnorePatterns: ["/node_modules/(?!vue-birth-datepicker)"]
+  transformIgnorePatterns: ["/node_modules/(?!vue-birth-datepicker)"],
+  moduleFileExtensions: ['js', 'json', 'node', 'vue'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: [ "text", 'cobertura','html'],
+  reporters: ["default",
+        ["jest-junit", {usePathForSuiteName: true, outputDirectory: 'coverage'}]
+      ],
+  testResultsProcessor: "jest-junit"
 }
